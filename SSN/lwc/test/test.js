2813a45dcf7f59dcbfd66e66dfbcf29c7a04dbab
AB <39646462+AkashBadnale@users.noreply.github.com>
2021-11-24 21:29:33 +0530

    Add files via upload

--- SSN/lwc/test/test.js
+++ SSN/lwc/test/test.js
@@ -4,6 +4,9 @@ import addTest from '@salesforce/apex/TestService.addTest';
 export default class Test extends LightningElement {
     firstName;
     lastName;
+    mobile;
+    dob;
+    email;
     
     nameChange(event){
         this.firstName = event.target.value;
@@ -11,9 +14,22 @@ export default class Test extends LightningElement {
     surnameChange(event){
         this.lastName = event.target.value;
     }
+    mobileChange(event){
+        this.mobile = event.target.value;
+    }
+    dobChange(event){
+        this.dob = event.target.value;
+    }
+    emailChange(event){
+        this.email = event.target.value;
+    }
 
     handleClick(event)
     {
-        addTest({name:this.firstName,surname:this.lastName});
+        addTest({name:this.firstName,
+                 surname:this.lastName,
+                 phone:this.mobile,
+                 birthday:this.dob,
+                 mail:this.email});
     }
 }
