0eac18eb2c195916348224e242b278c96711fd94
AB <39646462+AkashBadnale@users.noreply.github.com>
2021-11-25 12:49:53 +0530

    Delete SSN/lwc/get_all_persons directory

--- SSN/lwc/get_all_persons/get_all_persons.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-import getPersons from '@salesforce/apex/PersonController.getPersons';
-
-
-export default class Get_all_persons extends LightningElement {
-    @wire(getPersons) wiredPersons;
-}
